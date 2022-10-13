117b1e3083667d11b6ce5a3816734ac626636af8
nursultahn <nursultaan19982311@gmail.com>
2022-10-13 18:26:37 +0600

    Leetcode Solutions class

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from leetcode import Solutions as sl
+
+
 def print_hello_world(name):
     print(f'Hello {name}')
 
 
 if __name__ == '__main__':
-    print_hello_world('world')
+    nums = [1, 2, 3, 4]
+    operations = ["++X", "++X", "X++"]
+    val = sl.finalValueAfterOperations(operations=operations)
+    print(val)
